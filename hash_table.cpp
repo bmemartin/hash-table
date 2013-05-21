80426edd3a9e75fa81c7e423c36ab0ffc0a64633
Benjamin Martin <benjamin.martin987@gmail.com>
2013-05-21 18:40:10 +1000

    Empty functions added.

--- hash_table.cpp
+++ hash_table.cpp
@@ -2,7 +2,45 @@
 
 #include <cstdlib>
 
+HashTable::HashTable()
+{
 
+}
+
+HashTable::HashTable(const HashTable &other)
+{
+
+}
+
+HashTable::~HashTable()
+{
+
+}
+
+void HashTable::insert(string key, int val)
+{
+
+}
+
+void HashTable::remove(string key)
+{
+
+}
+
+int& HashTable::operator[](string key)
+{
+
+}
+
+HashTable& HashTable::operator=(const HashTable &other)
+{
+
+}
+
+inline void HashTable::clear()
+{
+
+}
 
 int HashTable::getHashVal(const char *key)
 {
